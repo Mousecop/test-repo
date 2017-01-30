a13b9a12f9d188aac7ddd53468fcb664a53afc2a
Dustwise <jared@dustwise.com>
2017-01-30 10:40:43 -0700

    Added yearOfBirth

--- starter-file.js
+++ starter-file.js
@@ -1,5 +1,7 @@
 function whoAmI(name, age){
+    var yearOfBirth = (2017 - age); 
     console.log("Hi, my ${name} is Chris, and I'm ${age} years old");
+    console.log("I was born in ${yearOfBirth}");
 }
 
 whoAmI(Jake, 23);
